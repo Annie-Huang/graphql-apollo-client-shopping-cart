fa7624dc3a92843feca543ff5b0b90efa9b86ba2
Annie Huang <annie.hn@gmail.com>
2024-07-27 16:49:00 +1000

    Copy StoreItems from https://pastebin.com/fF66w1qf across

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import './App.css';
 
+// https://pastebin.com/fF66w1qf
+const StoreItems = [
+  {
+    name: 'Cooper Codes Channel',
+    description: 'Buy the Cooper Codes!',
+    price: 10,
+    id: 1,
+  },
+  {
+    name: 'Shoes',
+    description: 'Cool casual shoes',
+    price: 50,
+    id: 2,
+  },
+  {
+    name: 'Bitcoin',
+    description: 'Buy some cyprotcurrency!',
+    price: 5,
+    id: 3,
+  },
+  {
+    name: 'JavaScript',
+    description: 'Buy the JS source code!',
+    price: 15,
+    id: 4,
+  },
+];
+
 function App() {
   return (
     <div className='App'>
