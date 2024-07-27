4ad2eee7a3c2e5581a84ba1709b9d40927820b85
Annie Huang <annie.hn@gmail.com>
2024-07-27 16:37:15 +1000

    Setup ApolloProvider and ChakraProvider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: CustomInMemoryCache, // Not defined yet.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
